a2923749c5e8f9e4379f617c271751c4670286b2
HannahLee777 <hannah.le1904@gmail.com>
2022-10-02 22:09:03 +1100

    Init

--- src/lession_07/CalculateEmployeeSalary.java
+++ src/lession_07/CalculateEmployeeSalary.java
@@ -4,8 +4,8 @@ public class CalculateEmployeeSalary {
     private static int NO_PER = 3;
     public static int NO_CONTRACTOR = 2;
     public static void main(String[] args) {
-        Employee employeePer = new Employee("permanent", 5000);
-        Employee employeeContractor = new Employee("contractor", 4000);
+        Employee employeePer = new Employee("permanent", 50000);
+        Employee employeeContractor = new Employee("contractor", 40000);
 
         int totalSalary = NO_PER*employeePer.getSalary() + NO_CONTRACTOR*employeeContractor.getSalary();
         System.out.println("Total salary: " + totalSalary);
